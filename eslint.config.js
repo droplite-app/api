5e522005c0561e311b60c3a9e4f8e0538da9eb02
Bahar Işıklı <baharisikli@outlook.com>
2024-08-15 16:13:30 +0300

    Remove .eslintrc.json and update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'eslint';
-
-export default defineConfig({
-  ignores: ['node_modules/', 'dist/'], // ignorePatterns yerine ignores
-  // diğer konfigürasyon ayarlarınız
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  rules: {
-    // kendi kurallarınız
-  },
-});
+module.exports = {
+    ignores: ['node_modules/', 'dist/'], 
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    rules: {
+     
+    },
+  };
