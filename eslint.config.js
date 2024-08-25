a4864ce8997e882035715162211972bf9f2d8db9
Bahar Işıklı <baharisikli@outlook.com>
2024-08-25 14:36:39 +0300

    eslint bug fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,18 @@
-const { ESLint } = require('eslint');
+const { ESLint } = require("eslint");
 
-module.exports = {
-  ignores: ['node_modules/', 'dist/'],
-  languageOptions: {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
+module.exports = [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      
     },
   },
-  plugins: {
-    '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  rules: {
-    semi: ['error', 'always'],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
-};
+];
