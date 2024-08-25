a395abcc6e7775b74b317f368a22e88311e77dc9
Bahar Işıklı <baharisikli@outlook.com>
2024-08-25 14:25:47 +0300

    Eslint changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,24 @@
+const { ESLint } = require('eslint');
+
 module.exports = {
-    ignores: ['node_modules/', 'dist/'], 
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignores: ['node_modules/', 'dist/'],
+  languageOptions: {
     parser: '@typescript-eslint/parser',
     parserOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
     },
-    rules: {
-     
-    },
-  };
+  },
+  plugins: {
+    '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
