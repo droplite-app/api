b0487ce6abb8e5b9f5a6c305e86d2ffc9006982e
Bahar Işıklı <baharisikli@outlook.com>
2024-09-12 12:55:06 +0300

    changes in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,20 @@
-// knexfile.js
 const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  development: {
-    client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT || 3306, 
-    },//COMMENT
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
   },
-  staging: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+  pool: {
+    min: 2,
+    max: 10,
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+  migrations: {
+    tableName: 'knex_migrations',
   },
 };
