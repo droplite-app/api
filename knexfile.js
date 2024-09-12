d24b250fd4ad29879cfd32007d73f6a0c171a656
Bahar Işıklı <baharisikli@outlook.com>
2024-09-12 12:32:34 +0300

    dotenv file changed

--- knexfile.js
+++ knexfile.js
@@ -1,34 +1,33 @@
 // knexfile.js
 const dotenv = require('dotenv');
-dotenv.config(); 
+dotenv.config();
 
 module.exports = {
-
   development: {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    }
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT || 3306, 
+    },
   },
-
   staging: {
     client: 'postgresql',
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
+      database: process.env.DB_NAME,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
@@ -37,15 +36,14 @@ module.exports = {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
+      database: process.env.DB_NAME,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: 'knex_migrations',
+    },
+  },
 };
