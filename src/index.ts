90081c7bdcc1f1feeae3fbac439410cd35d01c2a
Bahar Işıklı <baharisikli@outlook.com>
2024-09-13 16:13:18 +0300

    hashing password with bcrypt

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import db from './knex';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import { validate, setLocales, en } from 'robust-validator';
 
 dotenv.config();
@@ -16,7 +17,7 @@ const JWT_SECRET = process.env.JWT_SECRET || 'key';
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173' }));
 
-// Validation definitions for user creation and login
+// Validation definitions
 const createUserValidation = {
   email: 'required|email',
   password: 'required|min:6|max:50',
@@ -27,15 +28,27 @@ const loginValidation = {
   password: 'required|min:6|max:50',
 };
 
+// Password Hashing Configuration
+const saltRounds = 10;
+
 // Creating new users -endpoint
 app.post('/users', async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
+  // Validation for user creation
   const result = await validate({ email, password }, createUserValidation);
 
+  // Eğer result.errors varsa validasyon hatalarını döndür
+  if (Object.keys(result.errors).length > 0) {
+    return res.status(400).json({ message: result.errors });
+  }
+
   try {
+    // Hash the password before saving to the database
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     const [userId] = await db('users')
-      .insert({ email, password })
+      .insert({ email, password: hashedPassword })
       .returning('id');
     const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: '1h' });
     res
@@ -50,15 +63,28 @@ app.post('/users', async (req: Request, res: Response) => {
   }
 });
 
+// Login user - endpoint
 app.post('/login', async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
+  // Validation for login
   const result = await validate({ email, password }, loginValidation);
 
+  if (Object.keys(result.errors).length > 0) {
+    return res.status(400).json({ message: result.errors });
+  }
+
   try {
-    const user = await db('users').where({ email, password }).first();
+    const user = await db('users').where({ email }).first();
 
     if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Compare the hashed password with the provided password
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
