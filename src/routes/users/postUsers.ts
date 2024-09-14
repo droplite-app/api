604bfc355ec275e0a6efd69e7d1ee643a763f15c
Bahar Işıklı <baharisikli@outlook.com>
2024-09-14 19:32:16 +0300

    bug fix

--- src/routes/users/postUsers.ts
+++ src/routes/users/postUsers.ts
@@ -6,11 +6,8 @@ import { validate, setLocales, en } from 'robust-validator';
 import { SALT_ROUNDS } from '../../constants';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'key';
-const saltRounds = 10;
-
 
 setLocales(en);
-
 
 const createUserValidation = {
   email: 'required|email',
@@ -18,35 +15,38 @@ const createUserValidation = {
   full_name: 'required|min:3|max:100',
 };
 
-export const postUserHandler = async (req: Request, res: Response, next: NextFunction) => {
+export const postUserHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const { email, password, full_name } = req.body;
 
   // Validate the input fields
-  const result = await validate({ email, password, full_name }, createUserValidation);
+  const result = await validate(
+    { email, password, full_name },
+    createUserValidation,
+  );
 
   if (result.isInvalid) {
     return res.status(400).json({ message: result.errors });
   }
 
- 
-    // Check if the email already exists in the database
-    const existingUser = await db('users').where({ email }).first();
+  // Check if the email already exists in the database
+  const existingUser = await db('users').where({ email }).first();
 
-    if (existingUser) {
-      return res.status(409).json({ message: 'Email already exists' });
-    }
+  if (existingUser) {
+    return res.status(409).json({ message: 'Email already exists' });
+  }
 
-    // Hash the user's password
-    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  // Hash the user's password
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
-    
-    const [userId] = await db('users')
-      .insert({ email, password: hashedPassword, full_name })
-      .returning('id');
+  const [userId] = await db('users')
+    .insert({ email, password: hashedPassword, full_name })
+    .returning('id');
 
-    
-    const token = jwt.sign({ userId }, JWT_SECRET);
+  const token = jwt.sign({ userId }, JWT_SECRET);
 
-    res.status(201).json({token, userId });
-   
+  res.status(201).json({ token, userId });
 };
