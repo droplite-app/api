28b6b3cc41ea8d7c3f94d33543425692760b4d06
Bahar Işıklı <baharisikli@outlook.com>
2024-09-14 14:23:59 +0300

    full name connected

--- src/routes/users/postUsers.ts
+++ src/routes/users/postUsers.ts
@@ -14,13 +14,14 @@ setLocales(en);
 const createUserValidation = {
   email: 'required|email',
   password: 'required|min:6|max:50',
+  full_name: 'required|min:3|max:100', 
 };
 
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-  const { email, password } = req.body;
+  const { email, password, full_name } = req.body; 
 
-  
-  const result = await validate({ email, password }, createUserValidation);
+  // Validation
+  const result = await validate({ email, password, full_name }, createUserValidation);
 
   if (Object.keys(result.errors).length > 0) {
     return res.status(400).json({ message: result.errors });
@@ -33,13 +34,15 @@ router.post('/', async (req: Request, res: Response, next: NextFunction) => {
       return res.status(400).json({ message: 'Email already exists' });
     }
 
-    // Hash the password before saving to the database
+   
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
+    
     const [userId] = await db('users')
-      .insert({ email, password: hashedPassword })
+      .insert({ email, password: hashedPassword, full_name }) 
       .returning('id');
 
+    
     const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: '1h' });
 
     res.status(201).json({ message: 'User created successfully', token, userId });
