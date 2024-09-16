2c1926f4c93ac5ca07c4a11d2e3decdc41b55614
Bahar Işıklı <baharisikli@outlook.com>
2024-09-16 14:13:01 +0300

    testing config

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,24 @@
 const dotenv = require('dotenv');
 dotenv.config();
-//bug fix try
+
 module.exports = {
+  development:{
+    client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
+  },
   client: 'mysql2',
   connection: {
     host: process.env.DB_HOST,
@@ -17,4 +34,17 @@ module.exports = {
   migrations: {
     tableName: 'knex_migrations',
   },
+
+  
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:', 
+    },
+    useNullAsDefault: true, 
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+  
 };
